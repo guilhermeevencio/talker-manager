af161737486a8dad9174e3d06dfe26b2d1ff63da
Guilherme <guilhermeevencioluz@gmail.com>
2022-06-20 16:39:49 -0300

    refactor: talkerManager.routes.js

--- UseCases/searchTalker/SearchTalkerUseCase.js
+++ UseCases/searchTalker/SearchTalkerUseCase.js
@@ -3,7 +3,6 @@ const fs = require('fs');
 function searchTalkerUseCase(searchTerm) {
   const data = JSON.parse(fs.readFileSync('talker.json', 'utf-8'));
   const searchResult = data.filter((talk) => talk.name.match(searchTerm));
-  console.log(searchResult);
   return searchResult;
 }
 
--- routes/talkerManager.routes.js
+++ routes/talkerManager.routes.js
@@ -9,14 +9,25 @@ const searchTalkerController = require('../UseCases/searchTalker/SearchTalkerCon
 
 const talkerManagerRoutes = Router();
 
-talkerManagerRoutes.get('/talker', (req, res) => listTalkerController(req, res));
-talkerManagerRoutes.post('/talker', (req, res, next) => createTalkerController(req, res, next));
+talkerManagerRoutes
+  .get('/talker', (req, res) => listTalkerController(req, res));
+
+talkerManagerRoutes
+  .post('/talker', (req, res, next) => createTalkerController(req, res, next));
+
 talkerManagerRoutes
   .get('/talker/search', (req, res, next) => searchTalkerController(req, res, next));
-talkerManagerRoutes.get('/talker/:id', (req, res) => getTalkerByIdController(req, res));
-talkerManagerRoutes.put('/talker/:id', (req, res, next) => editTalkerController(req, res, next));
+
+talkerManagerRoutes
+  .get('/talker/:id', (req, res) => getTalkerByIdController(req, res));
+
+talkerManagerRoutes
+  .put('/talker/:id', (req, res, next) => editTalkerController(req, res, next));
+
 talkerManagerRoutes
   .delete('/talker/:id', (req, res, next) => deleteTalkerController(req, res, next));
-talkerManagerRoutes.post('/login', (req, res, next) => loginController(req, res, next));
+
+talkerManagerRoutes
+  .post('/login', (req, res, next) => loginController(req, res, next));
 
 module.exports = talkerManagerRoutes;
